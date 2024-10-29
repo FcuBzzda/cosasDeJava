b89838538d0e66e9617167db760781aef19d7ed7
FcuBzzda <163415438+FcuBzzda@users.noreply.github.com>
2024-10-29 12:08:59 -0300

    Create PersonaFisica.java

---
+++ PersonaFisica.java
@@ -0,0 +1,12 @@
+package ej19;
+
+public class PersonaFisica extends Persona {
+	private int DNI;
+
+	public PersonaFisica(String nombre, String direccion, int dNI) {
+		super(nombre, direccion);
+		DNI = dNI;
+	}
+	
+	
+}
