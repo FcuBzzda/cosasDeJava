47c873eb0530a2f244cedc370e75741116132ca8
FcuBzzda <163415438+FcuBzzda@users.noreply.github.com>
2024-11-05 12:09:53 -0300

    Create Persona.java

---
+++ Persona.java
@@ -0,0 +1,20 @@
+package ejercicio23;
+
+public abstract class Persona {
+	private String nombre;
+	private String direccion;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+}
